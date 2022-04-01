7d56cef8e15b020c5fa41f18abacce62118892c9
Stire <stire.ru@gmail.com>
2022-04-02 01:47:53 +0300

    added json to yaml conversion

--- src/main.rs
+++ src/main.rs
@@ -14,21 +14,32 @@ fn main() {
 
     let mut file = File::open(&options[0]).expect("Could not load file");
 
+    let ext = Path::new(&options[0]).extension().unwrap();
+
     let mut contents = String::new();
 
     file.read_to_string(&mut contents)
         .expect("Could not read file contents");
 
-    let json_value: serde_json::Value = serde_yaml::from_str(&contents).unwrap();
+    let res = match ext.to_str() {
+        Some("yaml") | Some("yml") => {
+            let value: serde_json::Value = serde_yaml::from_str(&contents).unwrap();
 
-    let res = serde_json::to_string_pretty(&json_value).unwrap();
+            serde_json::to_string_pretty(&value).unwrap()
+        }
+        Some("json") => {
+            let value: serde_yaml::Value = serde_json::from_str(&contents).unwrap();
+
+            serde_yaml::to_string(&value).unwrap()
+        }
+        _ => panic!("You forgot to specify this case!"),
+    };
 
     println!("{}", res);
 
     let new_path = Path::new(&options[1]);
-    let temp = new_path.display();
 
-    println!("{:?}", temp);
+    println!("{:?}", new_path.display());
 
     let prefix = new_path.parent().unwrap();
     create_dir_all(prefix).unwrap();
@@ -37,3 +48,13 @@ fn main() {
 
     writeln!(&mut file, "{}", res).unwrap();
 }
+
+// fn main() {
+//     let obj = json!({"foo":1,"bar":2});
+
+//     let buf = Vec::new();
+//     let formatter = serde_json::ser::PrettyFormatter::with_indent(b"    ");
+//     let mut ser = serde_json::Serializer::with_formatter(buf, formatter);
+//     obj.serialize(&mut ser).unwrap();
+//     println!("{}", String::from_utf8(ser.into_inner()).unwrap());
+// }
