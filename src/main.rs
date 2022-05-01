39ab5d77c7b6cf19f86b896e300215b8cd371b4e
Stire <stire.ru@gmail.com>
2022-05-01 18:34:31 +0300

    added dir mode

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use clap::{ArgEnum, Parser, Subcommand};
+use itertools::Itertools;
 use json5;
 use regex::Regex;
 use std::fs::{create_dir_all, File};
 use std::io::{Read, Write};
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use wax::Glob;
 
 extern crate serde;
 extern crate serde_json;
 extern crate serde_yaml;
-
-use clap::{ArgEnum, Parser, Subcommand};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -17,12 +18,25 @@ use clap::{ArgEnum, Parser, Subcommand};
 struct Args {
     #[clap(subcommand)]
     subcmd: SubCommand,
+
+    /// delete source file or not
+    #[clap(global = true, short, long)]
+    prune: bool,
+
+    #[clap(global = true, long, default_value_t = 2)]
+    indent: usize,
+
+    #[clap(global = true, long, arg_enum, default_value_t = IndentStyle::Space)]
+    indent_style: IndentStyle,
 }
 
 #[derive(Subcommand, Debug)]
 enum SubCommand {
     #[clap()]
     Single(Single),
+
+    #[clap()]
+    Dir(Directory),
 }
 
 /// Mode for converting a single file
@@ -35,12 +49,29 @@ struct Single {
     /// Output file
     #[clap(short, long, parse(try_from_str=valid_ext))]
     output: String,
+}
 
-    #[clap(long, default_value_t = 2)]
-    indent: usize,
+/// Mode for converting a all files in directory
+#[derive(Parser, Debug)]
+struct Directory {
+    /// root directory
+    #[clap(short, long)]
+    root: String,
 
-    #[clap(long, arg_enum, default_value_t = IndentStyle::Space)]
-    indent_style: IndentStyle,
+    /// all extensions to be converted
+    #[clap(arg_enum, short, long, multiple_occurrences(true))]
+    input: Vec<Extensions>,
+
+    /// resulting extension
+    #[clap(arg_enum, short, long)]
+    output: Extensions,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum, Hash)]
+enum Extensions {
+    Yaml,
+    Yml,
+    Json,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
@@ -61,61 +92,123 @@ fn valid_ext(s: &str) -> Result<String, String> {
 fn main() {
     let args = Args::parse();
 
-    match args.subcmd {
-        SubCommand::Single(Single {
-            input,
-            output,
-            indent,
-            indent_style: _,
-        }) => {
-            let mut input_file = File::open(&input).unwrap();
+    let Args {
+        indent,
+        indent_style,
+        subcmd,
+        prune,
+    } = args;
 
-            let mut input_data = String::new();
-            input_file.read_to_string(&mut input_data).unwrap();
+    match subcmd {
+        SubCommand::Single(Single { input, output }) => {
+            convert_file(indent_style, &input, &output, indent);
 
-            let input_ext = Path::new(&input).extension().unwrap().to_str().unwrap();
-            let output_ext = Path::new(&output).extension().unwrap().to_str().unwrap();
-
-            let mut output_data = match (input_ext, output_ext) {
-                ("yaml" | "yml", "json") => {
-                    let value: serde_json::Value = serde_yaml::from_str(&input_data).unwrap();
-
-                    serde_json::to_string_pretty(&value).unwrap()
-                }
-                ("json", "yaml" | "yml") => {
-                    let value: serde_yaml::Value = json5::from_str(&input_data).unwrap();
-
-                    serde_yaml::to_string(&value).unwrap()
-                }
-                _ => panic!("Unsupported extension"),
-            };
-
-            if indent != 2 {
-                let rg = Regex::new(r"\n(\s*)").unwrap();
-                let indent_str = format!("\n{}", "$1".repeat(indent / 2));
-                output_data = rg.replace_all(&output_data, indent_str).to_string();
+            if prune && input != output {
+                std::fs::remove_file(input).unwrap();
             }
+        }
 
-            let new_path = Path::new(&output);
+        SubCommand::Dir(Directory {
+            input,
+            output: out_ext,
+            root,
+        }) => {
+            let ext = input
+                .iter()
+                .unique()
+                .map(|x| extension_to_string(x))
+                .join(",");
 
-            let prefix = new_path.parent().unwrap();
-            create_dir_all(prefix).unwrap();
+            let files = get_files(&ext, &root);
 
-            let mut output_file = File::create(new_path).unwrap();
-            output_file.write_all(output_data.as_bytes()).unwrap();
+            files.iter().for_each(|file| {
+                let input = file.to_str().unwrap();
+                let base_ext = file.extension().unwrap().to_str().unwrap();
+                let output = input[..input.len() - base_ext.len()].to_owned()
+                    + &extension_to_string(&out_ext);
+
+                convert_file(indent_style, input, &output, indent);
+
+                if prune && input != output {
+                    std::fs::remove_file(file).unwrap();
+                }
+            })
         }
     }
 }
 
-// fn main() {
-//     let obj = json!({"foo":1,"bar":2});
+fn extension_to_string(x: &Extensions) -> String {
+    match x {
+        Extensions::Yaml => "yaml".to_owned(),
+        Extensions::Yml => "yml".to_owned(),
+        Extensions::Json => "json".to_owned(),
+    }
+}
 
-//     let buf = Vec::new();
-//     let formatter = serde_json::ser::PrettyFormatter::with_indent(b"    ");
-//     let mut ser = serde_json::Serializer::with_formatter(buf, formatter);
-//     obj.serialize(&mut ser).unwrap();
-//     println!("{}", String::from_utf8(ser.into_inner()).unwrap());
-// }
+fn convert_file(_: IndentStyle, input: &str, output: &str, indent: usize) {
+    let mut input_file = File::open(&input).unwrap();
+    let mut input_data = String::new();
+
+    input_file.read_to_string(&mut input_data).unwrap();
+
+    let input_ext = Path::new(&input).extension().unwrap().to_str().unwrap();
+    let output_ext = Path::new(&output).extension().unwrap().to_str().unwrap();
+
+    let mut output_data = match (input_ext, output_ext) {
+        ("yaml" | "yml", "json") => {
+            let value: serde_json::Value = serde_yaml::from_str(&input_data).unwrap();
+
+            serde_json::to_string_pretty(&value).unwrap()
+        }
+        ("json", "yaml" | "yml") => {
+            let value: serde_yaml::Value = json5::from_str(&input_data).unwrap();
+
+            serde_yaml::to_string(&value).unwrap()
+        }
+        ("yaml" | "yml", "yaml" | "yml") => {
+            let value: serde_yaml::Value = serde_yaml::from_str(&input_data).unwrap();
+
+            serde_yaml::to_string(&value).unwrap()
+        }
+        ("json", "json") => {
+            let value: serde_json::Value = json5::from_str(&input_data).unwrap();
+
+            serde_json::to_string(&value).unwrap()
+        }
+        t => panic!("Unsupported extension {:?}", t),
+    };
+
+    if indent != 2 {
+        let rg = Regex::new(r"\n(\s*)").unwrap();
+        let indent_str = format!("\n{}", "$1".repeat(indent / 2));
+        output_data = rg.replace_all(&output_data, indent_str).to_string();
+    }
+
+    let new_path = Path::new(&output);
+    let prefix = new_path.parent().unwrap();
+    create_dir_all(prefix).unwrap();
+    let mut output_file = File::create(new_path).unwrap();
+    output_file.write_all(output_data.as_bytes()).unwrap();
+}
+
+// function that takes file extension and root directory
+// and returns a list of files by glob in the directory
+// with the given extension
+fn get_files(ext: &str, root: &str) -> Vec<PathBuf> {
+    let mut files = Vec::new();
+
+    let expression = format!("**/*.{{{}}}", ext);
+
+    let glob = Glob::new(&expression).unwrap();
+
+    for entry in glob.walk(root, usize::MAX) {
+        let entry = entry.unwrap();
+
+        files.push(entry.path().to_owned());
+    }
+
+    files
+}
 
 // function to replace spaces at start of string with tabs
 // todo - make this work
@@ -143,6 +236,7 @@ mod tests {
 
     #[test]
     fn basic_tests() {
+        // assert_eq!(replace_spaces_with_tabs(" hello world",), "  hello world");
         assert_eq!(replace_spaces_with_tabs(" hello world",), "  hello world");
     }
 }
